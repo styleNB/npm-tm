0146d7778ab40a69b40f05f35f3b8a133a8b3263
handsome <yujiaming139147@vip.qq.com>
2021-02-07 23:21:12 +0800

    2021-2-7-删除image-comoress包

--- index.js
+++ index.js
@@ -1,29 +1,12 @@
-class imageCompress 
+import Lrz from 'lrz'
 
-_imageCompressor (event) {
-  var file = event
-  console.log('file.size111111', file.size)
-  if (file.type === 'image/jpeg') {
+/*eslint-disable*/
+export default class ImageCompress {
+  constructor (file) {
     return new Promise((resolve, reject) => {
-      new ImageCompressor(file, { quality: 0.2,
-        success: function (result) {
-          console.log('file.size2222222', result.size)
-          const formData = new FormData()
-          formData.append('file', result, result.name)
-          resolve(formData)
-        },
-        error: function (err) {
-          console.log('压缩图片错误jpg', err)
-        }
-      })
-    })
-  } else {
-    return new Promise((resolve, reject) => {
-      lrz(file, { quality: 0.4 })
+      Lrz(file, { quality: 0.4 })
         .then(function (rst) {
-          const formData = new FormData()
-          resolve(rst.formData)
-          console.log('file.size333333', rst.fileLen)
+          resolve(rst)
         }).catch(function (err) {
           console.log('压缩图片错误png', err)
         })
